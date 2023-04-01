4032e61f19f8f506b8df55554f017de40b0459dc
Rodion Kadyumov <krrkes@gmail.com>
2023-04-01 13:49:22 +0500

    k

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store'
 import './index.scss';
@@ -12,11 +12,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>   
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
